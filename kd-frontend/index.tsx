6ae0d635943a32c75bcfef417be00060fef77ad0
Zynotics <zyno3619@gmail.com>
2025-12-06 02:36:51 +0100

    Tailwind integration

--- kd-frontend/index.tsx
+++ kd-frontend/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css'; // AUSKOMMENTIERT oder ENTFERNT
+import './index.css'; // AUSKOMMENTIERT oder ENTFERNT
 
 const container = document.getElementById('root');
 
